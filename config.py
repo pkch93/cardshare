1b18e5744daa4341c0f0cab5428255f1e0f5938c
pkch93 <star_final@naver.com>
2018-12-26 15:53:55 +0900

    kakao 연동

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ S3_BUCKET_NAME = os.environ.get("S3_BUCKET")
 AWS_ACCESS_KEY_ID = os.environ.get("S3_KEY")
 AWS_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
 AWS_S3_RESION = os.environ.get("S3_RESION")
+
+# KAKAO KEY
+KAKAOLINK_KEY = os.environ.get("KAKAOLINK_KEY")
