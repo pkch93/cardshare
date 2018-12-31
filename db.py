5ae8f490da865fa117f57289b43956f8da976148
pkch93 <star_final@naver.com>
2018-12-31 10:10:45 +0900

    modify link

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app, db
+from cardsharespace import app, db
 
 migrate = Migrate(app, db)
 
@@ -9,4 +9,3 @@ manager.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
-
