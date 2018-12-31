52974535184d9af44fb2219be53fcad5b86c0f39
pkch93 <star_final@naver.com>
2018-12-31 22:04:24 +0900

    bug fixed css file

--- cardsharespace/static/js/form.js
+++ cardsharespace/static/js/form.js
@@ -44,7 +44,3 @@ var card = document.querySelector(".card");
 card.addEventListener("click", function() {
   card.classList.toggle("is-flipped");
 });
-var cardFace = document.querySelector(".card__face");
-var cardImg = document.querySelector(".card__img");
-var cardTitle = document.querySelector(".card__title");
-cardTitle.style.height = `${cardFace.offsetHeight - cardImg.height}px`;
